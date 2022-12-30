38d55483b5b2e0a500cc39b56e6ebbeb4320e7d3
marcio <marcioqui3@gmail.com>
2022-12-30 09:35:31 +0100

    jk

--- screens/AI.js
+++ screens/AI.js
@@ -1,51 +1,103 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {Text, View, Button} from 'react-native';
-import {Camera} from 'expo-camera';
-import * as Permissions from 'expo-permissions';
-import {cameraWithTensors} from '@tensorflow/tfjs-react-native';
-import * as MediaLibrary from 'expo-media-library';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Image,
+} from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Camera } from "expo-camera";
+import { shareAsync } from "expo-sharing";
+import * as MediaLibrary from "expo-media-library";
 
-const TensorCamera = cameraWithTensors(Camera);
-
-export default function App() {
-  const cameraRef = useRef();
-  const [hasPermission, setHasPermission] = useState(false);
+export default function AI() {
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  const [photo, setPhoto] = useState();
 
   useEffect(() => {
     (async () => {
-      const {status} = await Permissions.askAsync(Permissions.CAMERA, Permissionsa.MEDIA_LIBRARY);
-      setHasPermission(status === 'granted');
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
     })();
   }, []);
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>;
+  } else if (!hasCameraPermission) {
+    return (
+      <Text>
+        Permission for camera not granted. Please change this in settings.
+      </Text>
+    );
   }
 
-  const takePicture = async () => {
-    cameraRef.current.camera.takePictureAsync().then(result => {
-      MediaLibrary.saveToLibraryAsync(result.uri);
-    });
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false,
+    };
+
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
   };
 
+  if (photo) {
+    let sharePic = () => {
+      shareAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    let savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image
+          style={styles.preview}
+          source={{ uri: "data:image/jpg;base64," + photo.base64 }}
+        />
+        <Button title="Share" onPress={sharePic} />
+        {hasMediaLibraryPermission ? (
+          <Button title="Save" onPress={savePhoto} />
+        ) : undefined}
+        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View>
-      <TensorCamera
-        ref={cameraRef}
-        style={{
-          width: 600,
-          height: 600,
-        }}
-        autorender={true}
-        type={Camera.Constants.Type.back}
-      />
-      <View style={{marginTop: 50}}>
-        <Button onPress={() => takePicture()} title="Take Picture">
-        </Button>
+    <Camera style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        <Button title="Take Pic" onPress={takePic} />
       </View>
-    </View>
+      <StatusBar style="auto" />
+    </Camera>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "#fff",
+    alignSelf: "flex-end",
+  },
+  preview: {
+    alignSelf: "stretch",
+    flex: 1,
+  },
+});
